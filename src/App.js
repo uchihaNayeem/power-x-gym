d0c1d7b95286be940d138d08a3f95d39214b6808
Tanvir Ibn Touhid <tanvirbintouhid5230@gmail.com>
2020-11-15 23:40:30 +0600

    app structure made original

--- src/App.js
+++ src/App.js
@@ -3,21 +3,7 @@ import MembershipForm from "./form/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/form">
-            <MembershipForm />
-          </Route>
-          <Route path="/">
-            <h2>This is homepage</h2>
-            <Link to="/form">go to form</Link>
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
