bd4bf20a66a1f2e2b611e41851dec6b0e685f267
Tanvir Ibn Touhid <tanvirbintouhid5230@gmail.com>
2020-11-17 11:36:43 +0600

    redux added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MembershipForm from "./components/form/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Pricing from "./components/service/Pricing/Pricing";
 import OurClasses from "./components/service/OurClasses/OurClasses";
 import AdvanceGym from "./components/service/AdvanceGym/AdvanceGym";
@@ -25,19 +25,14 @@ const App = () => {
           <Route path="/pricing">
             <Pricing />
           </Route>
-          <Route path ="/advanceGym">
-            <AdvanceGym/>
+          <Route path="/advanceGym">
+            <AdvanceGym />
           </Route>
-          <Route path ="/home">
-            <Home/>
+          <Route path="/home">
+            <Home />
           </Route>
           <Route path="/">
-            <h2>this is home page.</h2>
-            <Link to="/form">form</Link>
-            <br />
-            <Link to="/pricing">pricing</Link>
-            <br />
-            <Link to="/services">our services</Link>
+            <Home />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+const initalState = 0;
+export default function rootReducer(state = initalState, action) {
+  switch (action.type) {
+    case 0:
+      break;
+
+    default:
+      return state;
+  }
+}
